2c7e6a15fc37390e98b0a1a6543b71d49a75efec
Yan Luo <yan_luo@ca.ibm.com>
2017-11-30 09:56:45 -0500

    Test
    Signed-off-by: Yan Luo <yan_luo@ca.ibm.com>

--- runtime/gc_base/StringTable.cpp
+++ runtime/gc_base/StringTable.cpp
@@ -936,6 +936,8 @@ j9gc_allocStringWithSharedCharData(J9VMThread *vmThread, U_8 *data, UDATA length
 						goto nomem;
 					}
 printf("YAN 2 2 about J9VMJAVALANGSTRING_SET_COMPRESSIONFLAG\n");
+void *pointer = (void*)-1;
+printf("YAN 3 %p\n", pointer);
 					J9VMJAVALANGSTRING_SET_COMPRESSIONFLAG(vmThread, stringClass, flag);
 				}
 			}
