283ee32898199499737cc16de7ff23598c11e899
Yan Luo <yan_luo@ca.ibm.com>
2017-12-04 14:08:56 -0500

    Refactor String getChars methods to skip bound checks
    Refactor String getChars into fast getCharsImpl methods that do not
    include bound checks. StringBuilder append(String) method which already
    ensures array bound safety will call the fast version.
    
    Also use the JIT recognized fast decompressedArrayCopy which does not
    generate arrayCopyBoundChecks instead of System.arrayCopy to perform
    the actual copy.
    
    Signed-off-by: Yan Luo <yan_luo@ca.ibm.com>

--- jcl/src/java.base/share/classes/java/lang/StringBuilder.java
+++ jcl/src/java.base/share/classes/java/lang/StringBuilder.java
@@ -664,7 +664,7 @@ public StringBuilder append (String string) {
 			ensureCapacityImpl(newLength);
 		}
 		
-		string.getChars(0, stringLength, value, currentLength);
+		string.getCharsImpl(0, stringLength, value, currentLength);
 		
 		count = newLength;
 	}
@@ -914,14 +914,10 @@ private void ensureCapacityImpl(int min) {
 	} else {
 		/*[IF Sidecar19-SE]*/
 		byte[] newData = new byte[newLength * 2];
-		
-		String.decompressedArrayCopy(value, 0, newData, 0, currentLength);
 		/*[ELSE]*/
 		char[] newData = new char[newLength];
-		
-		System.arraycopy(value, 0, newData, 0, currentLength);
 		/*[ENDIF]*/
-		
+		String.decompressedArrayCopy(value, 0, newData, 0, currentLength);
 		value = newData;
 	}
 	
